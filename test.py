00bd06f6e900ba9b6be91a091886f87ea3c6e44b
Jonas Bunsen <83568124+jbnsn@users.noreply.github.com>
2024-06-24 09:28:42 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,14 @@ import src.dbmarkenfarben as dbmf
 
 db_colors = dbmf.DeutscheBahnMarkenFarben()
 
-db_colors.print_colors()  # Print and return a list of available colours.
+# Print and return a list of the names of the available colours.
+db_colors.print_colors()
 
-db_colors.colors  # Return a dictionary of available colours.
+# Return a dictionary containing the names, shades
+# and html codes of the available colours.
+db_colors.colors
+
+[i for i in db_colors.colors if i[0] == 'red']  # ... only reds
 
 db_colors.get('red')  # Returns '#ec0016'
 db_colors.get('red', 200)  # Returns '#fcc8c3'
