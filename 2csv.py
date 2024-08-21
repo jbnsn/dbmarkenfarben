daa2165986159e670ea95700523d7910722d808f
Jonas Bunsen <83568124+jbnsn@users.noreply.github.com>
2024-08-21 16:00:43 +0200

    Update 2csv.py

--- 2csv.py
+++ 2csv.py
@@ -1,4 +1,11 @@
-"""Save a dataframe of all Deutsche Bahn brand colours as a CSV file."""
+"""
+Save a dataframe of all Deutsche Bahn brand colours as a CSV file.
+
+db_colors = pd.read_csv(
+    "https://github.com/jbnsn/dbmarkenfarben/blob/main/overview/colors.csv?raw=true",
+    )
+
+"""
 
 import dbmarkenfarben as dbmf
 import pandas as pd
